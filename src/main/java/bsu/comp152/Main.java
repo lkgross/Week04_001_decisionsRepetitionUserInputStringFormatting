7ca89d6600db9ac1a777a134d97089919477ff79
L1Gross <profgross@gmail.com>
2023-02-13 13:36:48 -0500

    Include user input and a formatted string

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,4 +1,5 @@
 package bsu.comp152;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,23 +8,43 @@ public class Main {
         printProceed("Y");
         powerWhile(16);
 
-        int n = 3;
-        int power = 1;
-        /*
-         * Declare and initialize a loop variable i with
-         * int i = 0;
-         * Continue the for loop so long as a loop-continuation
-         * condition is met. Here it's:
-         * i < n;
-         * Increment the loop variable at each pass through the loop with
-         * i++
-         */
-        for (int i = 0; i < n+1; i++){
-            System.out.println(i + " " + power);
-            power = 2*power;
+        int day = 1;
+        switch (day){
+            case 0:
+                System.out.println("Sun");
+                break;
+            case 1:
+                System.out.println("Mon");
+                break;
+            default:
+                System.out.println("ERROR");
         }
+        System.out.println("Laura".toLowerCase());
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("How many powers of two do you want?");
+        int numberOfPowers = keyboard.nextInt();
+        printTable(numberOfPowers);
+        System.out.println("What is your first name?");
+        // The method next reads a String, including the newline character.
+        String firstName = keyboard.next();
+        System.out.println("What is your last name?");
+        String lastName = keyboard.nextLine();
+        System.out.println("What is your age?");
+        int age = keyboard.nextInt();
+        System.out.printf("If the value is %b, then do " +
+                "the action", true);
+
+
     }
 
+    /**
+     * printProceed-
+     * A static method to print a message according to
+     * whether the value of proceed is "y", "n", or
+     * something else.
+     * @param proceed
+     */
     public static void printProceed(String proceed){
         // Don't use == for strings.
         // Don't do proceed == "y"!
@@ -50,5 +71,23 @@ public class Main {
         }
         System.out.println("An integer for log base 2 of " + n +
                 " is " + count);
+    }
+
+    public static void printTable(int numberRows){
+        int n = numberRows-1;
+        int power = 1;
+        /*
+         * Declare and initialize a loop variable i with
+         * int i = 0;
+         * Continue the for loop so long as a loop-continuation
+         * condition is met. Here it's:
+         * i < n;
+         * Increment the loop variable at each pass through the loop with
+         * i++
+         */
+        for (int i = 0; i < n+1; i++){
+            System.out.println(i + " " + power);
+            power = 2*power;
+        }
     }
 }
